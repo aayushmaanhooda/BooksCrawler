3fb48691afcdf62e3547bdd4cba73a970cfa3f5c
aayushmaanhooda <aayushmaan162@gmail.com>
2026-02-03 14:36:57 +1100

    first rule crawled

--- bookCrawl/bookCrawl/spiders/crawling_spider.py
+++ bookCrawl/bookCrawl/spiders/crawling_spider.py
@@ -1,7 +1,12 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-class CrawlingSpider(CrawlSpider):
+class BooksSpider(CrawlSpider):
     name = "books"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
 
-    
+    rules = (
+        Rule(LinkExtractor(allow="catalogue/category")),
+    ) 
+
