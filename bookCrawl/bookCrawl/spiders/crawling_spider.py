d47ccfa6a9a95e6bf97fb919cae1c2d2595e34f4
aayushmaanhooda <aayushmaan162@gmail.com>
2026-02-03 15:46:23 +1100

    added a output file

--- bookCrawl/bookCrawl/spiders/crawling_spider.py
+++ bookCrawl/bookCrawl/spiders/crawling_spider.py
@@ -1,12 +1,28 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+
 class BooksSpider(CrawlSpider):
     name = "books"
     allowed_domains = ["toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
 
+    # crawling rules
     rules = (
         Rule(LinkExtractor(allow="catalogue/category")),
-    ) 
+        Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item")
+    )
+
+    # scraping mechanism to do so action adnd get specific
+    def parse_item(self, response):
+        yield {
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".price_color::text").get(),
+            # "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ", "")
+            "availability": response.css(".availability::text").re_first(r"\d+")
+        }
+        
+
+
+
 
